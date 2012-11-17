df0bfd215086b0711d43812b72f3963eab05bcf1
Joanne Icken <ickenj@gmail.com>
2012-11-17 11:15:45 -0500

    commit all

--- js/calculator.js
+++ js/calculator.js
@@ -1,50 +1,53 @@
+// create helper function
+var $ = function (id) {
+		return document.getElementById(id);
+}
+
 function calculate() {
 	// get input from user
-	// var $list_price;		// Declare variable
 	var listPrice =
-		document.getElementById('list_price').valueAsNumber;
+		$('list_price').valueAsNumber;
 
 	var customerType =
-		document.getElementById('type').value;
- 
-	// Assign value to get reference to user input using document object method
-	$list_price = document.getElementById('list_price');
-	$list_price = $list_price.valueAsNumber; 	// new to html 5 - Get number value inputted using number object method
-	
-	// Calculate discount amount and discount price
-	// var $discount_percent = document.getElementById('discount_percent').valueAsNumber;
+		$('type').value;
+}
 
-	// Calculate discount percent
-	if (customerType == "R") {
-	    if (listPrice < 100)
-		discountPercent = 0;
-	    else if (listPrice >= 100 && listPrice < 250)
+// calculate discount percent function
+var discountPercent = discPercCalc(listPrice, customerType);
+
+// calculate discount amount function
+var discount = discAmountCalc(listPrice, discountPercent);
+
+// calculate discount price function
+var discountPrice = discPriceCalc(listPrice, discount);
+
+function discAmountCalc (listPrice, discountPercent) {
+        // Calculate discount amount 
+        var $discAmountCalc = (listPrice, discountPercent) {
+        return discAmount;
+}
+
+function discPriceCalc (listPrice, discount) {
+	var discPrice = listPrice - discount;
+	return discPrice;
+}
+
+function discPercCalc (listPrice, customerType);
+        // Calculate discount percent
+        if (customerType == "R") {
+            if (listPrice < 100)
+                discountPercent = 0;
+            else if (listPrice >= 100 && listPrice < 250)
                 discountPercent = 10;
             else if (listPrice >= 250)
                 discountPercent = 25;
         } else if (customerType == "C") {
-	  if (listPrice < 250)
-		discountPercent = 20;
+          if (listPrice < 250)
+                discountPercent = 20;
           else
-	        discountPercent = 30;
-	}
-	discountPercent = parseFloat(discountPercent);
-
-	// calculate discount amount and discount price
-	var $discount = $list_price * $discount_percent * .01;
-	var $discount_price = $list_price - $discount;
-	
-	//set output
-	document.getElementById('discount_percent').valueAsNumber = discountPercent;
-
-	$discount = $discount.toFixed(2);	// Convert number value to string value keeping 2 decimal places
-	$discount = '$' +$discount;			// Concatenate $ to string variable
-	var tempVar;						// Declare variable
-	tempVar = document.getElementById('discount');	// Set reference to output using document object method
-	tempVar.value = $discount;			// Set string value output using string object methods
-	
-	$discount_price = '$'+$discount_price.toFixed(2);
-	document.getElementById('discount_price').value = $discount_price;
-
-}	// end calculate
+                discountPercent = 30;
+        }
+        discountPercent = parseFloat(discountPercent);
+	return discountPercent;
+}
 
